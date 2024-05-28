617e75801756d27ff868ef5fd9ce1c814f31641b
Jacob Gillan <mail@finneh.xyz>
2024-05-28 12:27:36 +1000

    indentation cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,7 @@
 // It is responsible for initialising the navigation bar by adding an event listener to the hamburger icon.
 
 // Import the navInit function from navbar.js
-import {navInit} from "./navbar.js";
+import { navInit } from './navbar.js'
 
 // Call the navInit function to add the event listener to the hamburger icon
-navInit();
-
+navInit()
